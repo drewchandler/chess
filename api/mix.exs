92ce20d4d262769efab7162fb439649e3f9660e2
Drew Chandler <drew.chandler@dockyard.com>
2020-02-28 11:42:01 -0500

    Run ironman suit_up

--- api/mix.exs
+++ api/mix.exs
@@ -3,6 +3,18 @@ defmodule Chess.MixProject do
 
   def project do
     [
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      dialyzer: [
+        ignore_warnings: ".dialyzer_ignore.exs",
+        list_unused_filters: true,
+        plt_file: {:no_warn, "chess.plt"}
+      ],
       app: :chess,
       version: "0.1.0",
       elixir: "~> 1.5",
@@ -32,12 +44,17 @@ defmodule Chess.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:excoveralls, "~> 0.12.2", only: :test},
+      {:mix_test_watch, "~> 1.0.2", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0.0-rc.7", only: :dev, runtime: false},
+      {:earmark, "~> 1.4.3", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.21.3", only: :dev, runtime: false},
       {:phoenix, "~> 1.4.10"},
       {:phoenix_pubsub, "~> 1.1"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:credo, "~> 1.1.0", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.3.0-rc2", only: [:dev, :test], runtime: false}
     ]
   end
 end
