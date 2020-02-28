1a62cf9a3a508162a7dce80b3a395a950c6680fd
Drew Chandler <drew.chandler@dockyard.com>
2020-02-28 15:52:32 -0500

    Add a GenServer for games

---
+++ api/lib/chess_engine/game_session.ex
@@ -0,0 +1,30 @@
+defmodule ChessEngine.GameSession do
+  alias ChessEngine.Game
+
+  use GenServer
+
+  def init(players) do
+    players
+    |> Enum.shuffle()
+    |> (&{:ok, Game.new(players: &1)}).()
+  end
+
+  def handle_call({:move, player, from, to}, _from, game) do
+    case Game.move(game, player, from, to) do
+      {:ok, new_game} -> {:reply, {:ok, new_game}, new_game}
+      error -> {:reply, error, game}
+    end
+  end
+
+  def handle_call(:current_state, _from, game) do
+    {:reply, game, game}
+  end
+
+  def move(session, player, from, to) do
+    GenServer.call(session, {:move, player, from, to})
+  end
+
+  def current_state(session) do
+    GenServer.call(session, :current_state)
+  end
+end
