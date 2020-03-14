6ccf7338638e0ae2e5df470d2b2d5d70c5afff0d
Drew Chandler <drew.chandler@dockyard.com>
2020-03-14 10:01:31 -0400

    Render the board with the correct orientation for the player

--- ui/src/components/Board.tsx
+++ ui/src/components/Board.tsx
@@ -1,32 +1,39 @@
 import range from "lodash/range";
 import React, { FunctionComponent } from "react";
 import Square from "./Square";
-import { Board as BoardType } from "../models/Game";
+import { Board as BoardType, Color } from "../models/Game";
 
 interface Props {
   board: BoardType;
+  playerColor: Color;
   move: (from: number, to: number) => void;
 }
 
-const Board: FunctionComponent<Props> = ({ board, move }) => {
+const Board: FunctionComponent<Props> = ({ board, playerColor, move }) => {
+  let rows = range(0, 8);
+  let cols = range(0, 8);
+  if (playerColor === Color.White) {
+    rows = rows.reverse();
+  } else {
+    cols = cols.reverse();
+  }
+
   return (
     <div className="border border-gray-700 w-3/4-vmin h-3/4-vmin m-8 flex flex-wrap">
-      {range(0, 8)
-        .reverse()
-        .map(y =>
-          range(0, 8).flatMap(x => {
-            const index = y * 8 + x;
+      {rows.map(y =>
+        cols.flatMap(x => {
+          const index = y * 8 + x;
 
-            return (
-              <Square
-                key={index}
-                index={index}
-                piece={board[index]}
-                move={move}
-              />
-            );
-          })
-        )}
+          return (
+            <Square
+              key={index}
+              index={index}
+              piece={board[index]}
+              move={move}
+            />
+          );
+        })
+      )}
     </div>
   );
 };
--- ui/src/hooks/use-game.ts
+++ ui/src/hooks/use-game.ts
@@ -1,6 +1,6 @@
 import { useState, useCallback, useEffect } from "react";
 import useChannel from "./use-channel";
-import { Game, GameState } from "../models/Game";
+import { Game } from "../models/Game";
 
 interface GameDispatch {
   move(from: number, to: number): void;
@@ -8,8 +8,8 @@ interface GameDispatch {
 
 export default (
   name: string
-): { game: Game; error?: any; dispatch: GameDispatch } => {
-  const [game, setGame] = useState<Game>({ state: GameState.Loading });
+): { game?: Game; error?: any; dispatch: GameDispatch } => {
+  const [game, setGame] = useState<Game | undefined>();
   const onJoin = useCallback(payload => setGame(payload.game), [setGame]);
   const { error, channel } = useChannel(`game:${name}`, onJoin);
   const dispatch = {
