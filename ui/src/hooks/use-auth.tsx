be455a57727f34688cbb16d7a86c7fa0c615f734
Drew Chandler <drew.chandler@dockyard.com>
2020-03-27 12:53:23 -0400

    Store user in localStorage

--- ui/src/hooks/use-auth.tsx
+++ ui/src/hooks/use-auth.tsx
@@ -2,7 +2,8 @@ import React, {
   createContext,
   FunctionComponent,
   useState,
-  useContext
+  useContext,
+  useEffect
 } from "react";
 import useSocket from "./use-socket";
 
@@ -18,17 +19,27 @@ interface AuthContextValue {
 
 const authContext = createContext<AuthContextValue | undefined>(undefined);
 
+const USER_LOCAL_STORAGE_KEY = "user";
+
 export const ProvideAuth: FunctionComponent = ({ children }) => {
-  const [user, setUser] = useState<User | undefined>(undefined);
+  const [user, setUser] = useState<User | undefined>(() => {
+    const user = window.localStorage.getItem(USER_LOCAL_STORAGE_KEY);
+
+    return user && JSON.parse(user);
+  });
   const { connect, disconnect } = useSocket();
+  useEffect(() => user && connect(user.username), []); // eslint-disable-line react-hooks/exhaustive-deps
 
   const login = (username: string) => {
-    setUser({ username });
+    const user = { username };
+    window.localStorage.setItem(USER_LOCAL_STORAGE_KEY, JSON.stringify(user));
+    setUser(user);
     connect(username);
   };
 
   const logout = () => {
     setUser(undefined);
+    window.localStorage.removeItem(USER_LOCAL_STORAGE_KEY);
     disconnect();
   };
 
