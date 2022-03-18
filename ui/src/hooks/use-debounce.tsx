30bef4cc70f5d3d858b712cb3f9e681775359100
Drew Chandler <drew.chandler@dockyard.com>
2022-03-18 17:10:51 -0400

    Upgrade ui deps

--- ui/src/hooks/use-debounce.tsx
+++ ui/src/hooks/use-debounce.tsx
@@ -1,10 +1,11 @@
-import { debounce, Cancelable } from "lodash";
+import type { DebouncedFunc } from "lodash";
+import debounce from "lodash/debounce";
 import { useCallback, DependencyList } from "react";
 
 export const useDebounce = <T extends (...args: any[]) => any>(
   func: T,
   delay: number,
   deps: DependencyList
-): T & Cancelable => {
+): DebouncedFunc<T> => {
   return useCallback(debounce(func, delay), deps);
 };
