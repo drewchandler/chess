1612ed4b5251d3eeb7ffa3a96ba59a0a12d29afd
Drew Chandler <drew.chandler@dockyard.com>
2022-03-18 14:24:10 -0400

    Get rid of excessive pipe

--- api/lib/chess_web/serializers/position.ex
+++ api/lib/chess_web/serializers/position.ex
@@ -1,6 +1,5 @@
 defimpl Jason.Encoder, for: [Chess.Rules.Position] do
   def encode(position, _opts) do
-    (position.y * 8 + position.x)
-    |> Jason.Encode.integer()
+    Jason.Encode.integer(position.y * 8 + position.x)
   end
 end
