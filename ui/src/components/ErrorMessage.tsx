3a09481d644886971e0269b8b6bedd6f681149b5
Drew Chandler <drew.chandler@dockyard.com>
2022-04-15 15:33:06 -0400

    Remove UI

--- ui/src/components/ErrorMessage.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FunctionComponent } from "react";
-
-interface Props {
-  error: string;
-}
-
-export const ErrorMessage: FunctionComponent<Props> = ({ error }) => (
-  <span className="text-lg text-red-500 py-4">{error}</span>
-);
