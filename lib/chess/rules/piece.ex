1227a81155ba24770698057e59cd6bdd9522d138
Drew Chandler <drew.chandler@dockyard.com>
2022-05-27 12:33:15 -0400

    Remove old Jason.Encoder code

--- lib/chess/rules/piece.ex
+++ lib/chess/rules/piece.ex
@@ -1,6 +1,5 @@
 defmodule Chess.Rules.Piece do
   @enforce_keys [:type, :color]
-  @derive Jason.Encoder
   defstruct [:type, :color]
 
   def new(type, color), do: %__MODULE__{type: type, color: color}
